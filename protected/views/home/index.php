ea3027a78c02c8d60db73fa6d362347d5626172a
Lycsona <lycsona@gmail.com>
2015-11-29 21:32:21 +0200

    Initial commit

---
+++ protected/views/home/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	<title>Bootstrap 101 Template</title>
+	<!-- Bootstrap -->
+	<?php
+	Yii::app()->clientScript->registerScriptFile(Yii::app()->baseUrl . '/js/bootstrap.min.js', CClientScript::POS_END);
+	Yii::app()->clientScript->registerScriptFile(Yii::app()->baseUrl . '/js/jquery-2.1.4.min.js', CClientScript::POS_END);
+	Yii::app()->clientScript->registerCssFile(Yii::app()->baseUrl . '/css/bootstrap.min.css');
+	?>
+</head>
+<body>
+
+
+<h1>sdcdscsd</h1>
+
+</body>
+</html>
